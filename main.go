fe3f92edef97fefa6b0f226c4ed561107cca91cf
Arian van Putten <arian.vanputten@gmail.com>
2021-09-25 15:36:55 +0200

    Virtualhost the client

--- main.go
+++ main.go
@@ -8,8 +8,7 @@ import (
 )
 
 func main() {
-	// TODO replace with our oidc server
-	oauthclient := oauthclient.New("https://wire.okta.com", "localhost")
-	oauthClientHttpServer := http.Server{Handler: &oauthclient, Addr: "[::]:8080"}
-	log.Fatal(oauthClientHttpServer.ListenAndServe())
+	oauthclient := oauthclient.New("https://wire.okta.com", "client.localhost")
+	http.Handle("client.localhost/", &oauthclient)
+	log.Fatal(http.ListenAndServe("[::]:8080", nil))
 }
