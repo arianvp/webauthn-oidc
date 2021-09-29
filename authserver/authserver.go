45397fbf5e836a7453751fb8648602c308e4e379
Arian van Putten <arian.vanputten@gmail.com>
2021-09-30 01:32:41 +0200

    Communicate PKCE support in metadata

--- authserver/authserver.go
+++ authserver/authserver.go
@@ -26,19 +26,20 @@ const (
 )
 
 type OpenidConfiguration struct {
-	Issuer                        string                          `json:"issuer"`
-	RegistrationEndpoint          string                          `json:"registration_endpoint"`
-	AuthEndpoint                  string                          `json:"authorization_endpoint"`
-	TokenEndpoint                 string                          `json:"token_endpoint"`
-	JWKSURI                       string                          `json:"jwks_uri"`
-	UserinfoEndpoint              string                          `json:"userinfo_endpoint,omitempty"`
-	SupportedAlgs                 []oidc.Alg                      `json:"id_token_signing_alg_values_supported"`
-	SupportedScopes               []string                        `json:"scopes_supported"`
-	SubjectTypesSupported         []string                        `json:"subject_types_supported"`
-	ResponseTypesSupported        []string                        `json:"response_types_supported"`
-	GrantTypesSupported           []string                        `json:"grant_types_supported"`
-	CodeChallengeMethodsSupported []string                        `json:"code_challenge_methods_supported"`
-	ACRValuesSupported            []protocol.ConveyancePreference `json:"acr_values_supported"`
+	Issuer                            string                          `json:"issuer"`
+	RegistrationEndpoint              string                          `json:"registration_endpoint"`
+	AuthEndpoint                      string                          `json:"authorization_endpoint"`
+	TokenEndpoint                     string                          `json:"token_endpoint"`
+	JWKSURI                           string                          `json:"jwks_uri"`
+	UserinfoEndpoint                  string                          `json:"userinfo_endpoint,omitempty"`
+	SupportedAlgs                     []oidc.Alg                      `json:"id_token_signing_alg_values_supported"`
+	SupportedScopes                   []string                        `json:"scopes_supported"`
+	SubjectTypesSupported             []string                        `json:"subject_types_supported"`
+	ResponseTypesSupported            []string                        `json:"response_types_supported"`
+	GrantTypesSupported               []string                        `json:"grant_types_supported"`
+	CodeChallengeMethodsSupported     []string                        `json:"code_challenge_methods_supported"`
+	ACRValuesSupported                []protocol.ConveyancePreference `json:"acr_values_supported"`
+	TokenEndpointAuthMethodsSupported []string                        `json:"token_endpoint_auth_methods_supported"`
 }
 
 type AuthorizationServer struct {
@@ -83,19 +84,20 @@ func New(rpID string, origin string, privateKey *ecdsa.PrivateKey, cookieKeys []
 	}
 
 	server.config = &OpenidConfiguration{
-		Issuer:                        origin,
-		RegistrationEndpoint:          origin + register,
-		AuthEndpoint:                  origin + authorize,
-		TokenEndpoint:                 origin + token,
-		JWKSURI:                       origin + wellKnownJwks,
-		UserinfoEndpoint:              origin + userinfo,
-		SupportedAlgs:                 []oidc.Alg{oidc.EdDSA, oidc.ES256},
-		SupportedScopes:               []string{"openid"},
-		SubjectTypesSupported:         []string{"pairwise"},
-		ResponseTypesSupported:        []string{"code"},
-		GrantTypesSupported:           []string{"authorization_code"},
-		CodeChallengeMethodsSupported: []string{"S256"},
-		ACRValuesSupported:            []protocol.ConveyancePreference{},
+		Issuer:                            origin,
+		RegistrationEndpoint:              origin + register,
+		AuthEndpoint:                      origin + authorize,
+		TokenEndpoint:                     origin + token,
+		JWKSURI:                           origin + wellKnownJwks,
+		UserinfoEndpoint:                  origin + userinfo,
+		SupportedAlgs:                     []oidc.Alg{oidc.EdDSA, oidc.ES256},
+		SupportedScopes:                   []string{"openid"},
+		SubjectTypesSupported:             []string{"pairwise"},
+		ResponseTypesSupported:            []string{"code"},
+		GrantTypesSupported:               []string{"authorization_code"},
+		CodeChallengeMethodsSupported:     []string{"S256"},
+		ACRValuesSupported:                []protocol.ConveyancePreference{},
+		TokenEndpointAuthMethodsSupported: []string{"none"},
 	}
 
 	server.Handle(openidConfiguration, http.HandlerFunc(server.handleOpenidConfiguration))
