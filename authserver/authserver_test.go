faa0c165060d720e04cb0b4c27785199f7824386
Arian van Putten <arian.vanputten@gmail.com>
2021-10-25 13:16:20 +0200

    Add (failing) smoketest

---
+++ authserver/authserver_test.go
@@ -0,0 +1,38 @@
+package authserver
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/rsa"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestAllEndpointsReachable(t *testing.T) {
+	rpID := "localhost"
+	origin := "https://localhost:6443"
+	ecdsaKey, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	rsaKey, _ := rsa.GenerateKey(rand.Reader, 2048)
+	cookieKey := make([]byte, 64)
+	rand.Reader.Read(cookieKey)
+	clientSecretKey := make([]byte, 64)
+	rand.Reader.Read(clientSecretKey)
+	authserver := New(rpID, origin, ecdsaKey, rsaKey, [][]byte{cookieKey}, clientSecretKey)
+
+	for _, path := range []string{openidConfigurationPath, oauthAuthorizationServerPath, register, authorize, token, userinfo, wellKnownJwks} {
+		t.Run(path, func(t *testing.T) {
+			recorder := httptest.NewRecorder()
+			req := httptest.NewRequest("GET", origin+path, nil)
+
+			authserver.ServeHTTP(recorder, req)
+			resp := recorder.Result()
+			if resp.StatusCode == http.StatusNotFound {
+				t.Errorf("Expected endpoint %s to be found", origin+path)
+			}
+
+		})
+	}
+
+}
