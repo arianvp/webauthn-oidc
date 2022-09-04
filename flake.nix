c189aca8d76600d230af527503b278da5e327b08
Arian van Putten <arian.vanputten@gmail.com>
2022-09-04 19:08:15 +0200

    Delete test and fix

--- flake.nix
+++ flake.nix
@@ -55,7 +55,7 @@
           pname = "webauthn-oidc";
           version = "0.0.1";
           src =  pkgs.lib.cleanSource ./.;
-          vendorHash =  "sha256-+uBLqlp/xjOXLq8zQgtkG6MUTbNIJQtkRGfMhqSfIhM="; 
+          vendorHash = "sha256-qzjWUzykpTZJ+Vnx04+iafNxNIMIhOOOlXSUDniXrsg=";
         };
 
         devShell = with pkgs; mkShell {
